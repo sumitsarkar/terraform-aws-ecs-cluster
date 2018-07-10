9a0c73da1e9e2fc0c627f222d57026b0ce2ffe5d
Sumit Sarkar <ssarkar@cimpress.com>
2018-07-10 11:17:56 +0530

    FIX: Egress rule

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "template_file" "instance_role_policy" {
 }
 
 module "container_service_cluster" {
-  source = "git::https://github.com/sumitsarkar/terraform-aws-ecs-cluster-multi-instance?ref=1.2.0"
+  source = "git::https://github.com/sumitsarkar/terraform-aws-ecs-cluster-multi-instance?ref=1.2.1"
 
   vpc_id               = "${var.vpc_id}"
   region               = "${var.region}"
